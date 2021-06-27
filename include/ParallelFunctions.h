9c78d54f42f162624b748ddadebae969dd8b5025
yatiyr <dereeren@yahoo.com>
2021-06-28 02:58:25 +0300

    started implementing parallel part

--- include/ParallelFunctions.h
+++ include/ParallelFunctions.h
@@ -1,5 +1,16 @@
 #ifndef __PARALLEL_FUNCTIONS_H__
 #define __PARALLEL_FUNCTIONS_H__
 
+#define MANAGER_RANK 0
+
+#define MANAGER_TAG 1
+#define WORKER_TAG 2
+
+
+inline void gauss_seidel_parallel(double *A, double *b, double *x, size_t size, size_t maxIterations, double tolerance)
+{
+    
+}
+
 
 #endif
